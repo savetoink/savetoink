48578b0244f203a825eaa02255c153aaa45e2452
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-17 08:19:54 -0400

    webapp: better error handling

--- cmd/webapp/src/hooks.server.ts
+++ cmd/webapp/src/hooks.server.ts
@@ -1,6 +1,12 @@
 import type { Handle } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+import { PUBLIC_API_URL } from '$env/static/public';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (!PUBLIC_API_URL) {
+		error(500, 'PUBLIC_API_URL is not defined');
+	}
+
 	const jwt = event.cookies.get('jwt');
 	event.locals.jwt = jwt;
 
