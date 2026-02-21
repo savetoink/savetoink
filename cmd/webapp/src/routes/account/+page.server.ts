c6943dfb5322be1a97efaa6b780e5ea23d8a238f
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-21 15:05:44 -0400

    webapp: fix

--- cmd/webapp/src/routes/account/+page.server.ts
+++ cmd/webapp/src/routes/account/+page.server.ts
@@ -9,6 +9,7 @@ export const load: PageServerLoad = async ({ fetch, locals, cookies }) => {
 			const profile = await GET(fetch, '/v1/user/profile', locals.jwt);
 			return { ...profile, jwt: getJwtCookie(cookies) };
 		} catch (err) {
+			deleteJwtCookie(cookies);
 			console.error('failed to load user profile', err);
 		}
 	}
