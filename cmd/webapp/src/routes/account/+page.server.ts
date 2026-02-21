34a61af2fa6881e3b6a9c9d534155711076eccc3
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-21 14:04:10 -0400

    webapp: fix account page when jwt is invalid

--- cmd/webapp/src/routes/account/+page.server.ts
+++ cmd/webapp/src/routes/account/+page.server.ts
@@ -5,7 +5,11 @@ import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, locals }) => {
 	if (locals.jwt) {
-		return await GET(fetch, '/v1/user/profile', locals.jwt);
+		try {
+			return await GET(fetch, '/v1/user/profile', locals.jwt);
+		} catch (err) {
+			console.error('failed to load user profile', err);
+		}
 	}
 };
 
