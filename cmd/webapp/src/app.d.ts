0920afd4a5d5e2ef7000844536cc8a7f77cef08b
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-24 03:29:34 -0400

    webapp: cleanups

--- cmd/webapp/src/app.d.ts
+++ cmd/webapp/src/app.d.ts
@@ -1,19 +1,7 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
 		interface Locals {
 			jwt?: string;
-		}
-
-		interface PageData {
-			form?: {
-				error?: string;
-			};
-		}
-
-		interface Error {
-			message: string;
 		}
 	}
 }
