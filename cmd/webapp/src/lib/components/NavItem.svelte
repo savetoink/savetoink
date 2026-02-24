f2ae7cdb4c51b5277c66953323f95efd260196f9
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-02-24 14:31:54 -0400

    webapp: fix favorite navigation

--- cmd/webapp/src/lib/components/NavItem.svelte
+++ cmd/webapp/src/lib/components/NavItem.svelte
@@ -3,8 +3,17 @@
 	import { page } from '$app/state';
 
 	let { link, text } = $props();
+
+	const linkUrl = $derived(
+		new URL(link, typeof window !== 'undefined' ? window.location.origin : 'http://example.com')
+	);
 </script>
 
 <li>
-	<a href={resolve(link)} aria-current={page.url.pathname === link ? 'page' : undefined}>{text}</a>
+	<a
+		href={resolve(link)}
+		aria-current={page.url.pathname === linkUrl.pathname && page.url.search === linkUrl.search
+			? 'page'
+			: undefined}>{text}</a
+	>
 </li>
