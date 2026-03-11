ec068ac0f33703ce15fdc2c7546246a1fd517625
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-03-11 15:57:52 -0400

    feat(website): add robots and sitemap

--- frontend/website/astro.config.mjs
+++ frontend/website/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
 	site: 'https://www.saveto.ink',
-	output: 'static'
+	output: 'static',
+	integrations: [sitemap()]
 });
