c8ebb4f0a50ed7b51b30bb0b9b38525de91ebbaa
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-03-12 02:49:47 -0400

    fix(website): downgrade @astrojs/sitemap to fix builds

--- frontend/website/astro.config.mjs
+++ frontend/website/astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
+// NOTICE: seems that >3.6.0 is buggy and won't build
+// see https://github.com/savetoink/savetoink/issues/79
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
