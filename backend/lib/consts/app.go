8f6b5473db4daefae21199eebebc0c6c079d9fd5
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-03-12 10:17:30 -0400

    chore: bump versions to 0.27.0-dev+03121417.0a8a86f

--- backend/lib/consts/app.go
+++ backend/lib/consts/app.go
@@ -3,7 +3,7 @@ package consts
 
 import "time"
 
-var version = "0.27.0-dev+03120650.c8ebb4f"
+var version = "0.27.0-dev+03121417.0a8a86f"
 
 // Version returns the current version of the savetoink application.
 func Version() *string {
